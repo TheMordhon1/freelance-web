3fcf00d06c7b676249c224aeb0692818f827ec0d
themordhon <romdhonm12@gmail.com>
2022-01-09 21:47:08 +0700

    unique key on features section

--- components/FeaturesCard.jsx
+++ components/FeaturesCard.jsx
@@ -3,26 +3,31 @@ import Link from "next/link";
 export default function FeaturesCard() {
   const itemFeatures = [
     {
+      id: 1,
       title: "Work Load",
       desc: "Outward clothes promise at gravity do excited. Sufficient particular impossible by reasonable oh expression is...",
       href: "/features/work-lead",
     },
     {
+      id: 2,
       title: "Time Tracking",
       desc: "Outward clothes promise at gravity do excited. Sufficient particular impossible by reasonable oh expression is...",
       href: "/features/time-tracking",
     },
     {
+      id: 3,
       title: "Collaborations",
       desc: "Outward clothes promise at gravity do excited. Sufficient particular impossible by reasonable oh expression is...",
       href: "/features/collaborations",
     },
     {
+      id: 4,
       title: "Fast Money",
       desc: "Outward clothes promise at gravity do excited. Sufficient particular impossible by reasonable oh expression is...",
       href: "/features/fast-money",
     },
     {
+      id: 5,
       title: "Reporting",
       desc: "Outward clothes promise at gravity do excited. Sufficient particular impossible by reasonable oh expression is...",
       href: "/features/reporting",
@@ -31,7 +36,7 @@ export default function FeaturesCard() {
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 text-center">
       {itemFeatures.map((item) => (
-        <>
+        <div key={item.id}>
           <Link href={item.href}>
             <div className="relative py-8 px-5 bg-white flex flex-col gap-5 cursor-pointer hover:shadow-black transition-all hover:scale-105">
               <h3 className="text-black gill-semibold text-xl">{item.title}</h3>
@@ -41,7 +46,7 @@ export default function FeaturesCard() {
               </span>
             </div>
           </Link>
-        </>
+        </div>
       ))}
     </div>
   );
