0b8a6ec7b7bb942508f0788698275a1fbbf5789e
themordhon <romdhonm12@gmail.com>
2022-01-09 16:33:26 +0700

    fix z-index

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ import heroImage from "/public/hero-img.png";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col md:flex-row  items-center w-full justify-center h-[100vh] md:h-[calc(110vh-126px)] 2xl:h-[80vh]">
+    <div className="flex flex-col md:flex-row  items-center w-full h-[100vh] md:h-[calc(110vh-126px)] 2xl:h-[80vh]">
       <div className=" text-left gill-thin mt-20 md:mt-0  text-black w-full md:w-6/12 lg:w-7/12 md:order-1 order-2">
         <h1 className="w-1/4 text-6xl sm:text-5xl lg:w-1/2 lg:text-6xl xl:text-8xl">
           Providing Best
@@ -16,7 +16,7 @@ export default function Hero() {
           Letter of on become he tended active enable to. Vicinity relation
           sensible sociable surprise screened no up as.
         </p>
-        <div className="flex flex-col sm:flex-row w-full text-center gap-5 mt-10 xl:text-2xl">
+        <div className="flex relative z-10 flex-col sm:flex-row w-full text-center gap-5 mt-10 xl:text-2xl">
           <Link href="#">
             <div className="border border-blue py-2 px-10 hover:bg-blue/100 cursor-pointer bg-blue/80 text-white/100 transition-all">
               Get Started
@@ -35,7 +35,7 @@ export default function Hero() {
           className="static hidden md:block md:absolute md:-left-20 md:w-1/2 md:top-32 lg:-left-48 lg:top-32  lg:w-6/12"
           alt=""
         />
-        <div className="w-1/2 mx-auto -z-10 opacity-30 md:opacity-100 mt-5 absolute md:w-full md:top-20 lg:top-0 right-0 ">
+        <div className="w-1/2 mx-auto -z-0 opacity-30 md:opacity-100 mt-5 absolute right-0 md:-right-20 md:w-full md:top-20 lg:top-0 lg:right-0 ">
           <Image src={heroImage} />
         </div>
         <img
